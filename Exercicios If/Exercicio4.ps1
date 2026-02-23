17d872ef9563b4e299b7abf9d43c192f19604c31
Daniel Santos <Daniel.Santos.T0132015@edu.atec.pt>
2026-02-23 14:20:41 +0000

    Entrega dos exercicos

---
+++ Exercicios If/Exercicio4.ps1
@@ -0,0 +1,9 @@
+﻿$saldo = [double](Read-Host "Saldo Inicial")
+$cheque = [double](Read-Host "Valor do Cheque")
+
+if ($cheque -le $saldo) {
+    $novoSaldo = $saldo - $cheque
+    Write-Output "Cheque descontado, saldo: $novoSaldo"
+} else {
+    Write-Output "O cheque não pode ser descontado porque tem Saldo Insuficiente"
+}
---
+++ Exercicios If/Exercicio5.ps1
@@ -0,0 +1,12 @@
+﻿$valor1 = [int](Read-Host "numero1")
+$valor2 = [int](Read-Host "numero2")
+$valor3 = [int](Read-Host "numero")
+
+$lista = @($valor1, $valor2, $valor3)
+$crescente = $lista | Sort-Object
+$decrescente = $lista | Sort-Object -Descending
+
+Write-Output "Crescente: $($crescente -join ', ')"
+Write-Output "Decrescente: $($decrescente -join ', ')"
+
+# (-join ', ')" - Faz resultado aparecer na mesma linha separado por virgulas
