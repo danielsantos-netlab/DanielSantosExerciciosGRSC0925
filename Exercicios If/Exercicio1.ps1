dbf656be3ec471141c36160ca33813d658dead30
Daniel Santos <Daniel.Santos.T0132015@edu.atec.pt>
2026-02-23 13:52:16 +0000

    Entrega dos exercicos

--- Exercicios If/Exercicio1.ps1
+++ Exercicios If/Exercicio1.ps1
@@ -4,5 +4,6 @@ $horas = [Math]::Floor($totalSegundos / 3600)
 $restoHoras = $totalSegundos % 3600
 $minutos = [Math]::Floor($restoHoras / 60)
 $segundos = $restoHoras % 60
+# Floor - Para devolver só o que está antes da virgula (1,9 - devolve 1)
 
 Write-Output "Saída esperada: $horas hora), $minutos minutos e $segundos segundos"
