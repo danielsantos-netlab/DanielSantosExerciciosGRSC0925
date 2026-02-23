cfefcf1e062e1fbe964cf5919c88cd29a598a352
Daniel Santos <Daniel.Santos.T0132015@edu.atec.pt>
2026-02-23 11:50:39 +0000

    Entrega dos exercicos

---
+++ Exercicios Match/Exercicio7.ps1
@@ -0,0 +1,12 @@
+﻿$categoria = Read-Host "Categoria (eletrônico/alimento)"
+[double]$preco = Read-Host "Preço"
+
+$saida = switch ($categoria.Trim().ToLower()) {
+    "eletrônico" {
+        if ($preco -gt 1000) { "Produto de luxo" }
+        else { "Produto comum" }
+    }
+    "alimento" { "Produto alimentar" }
+}
+
+Write-Output $saida
---
+++ Exercicios Match/Exercicio8.ps1
@@ -0,0 +1,16 @@
+﻿$operacao = Read-Host "Operação (soma/subtrai/multiplica/divide)"
+[double]$num1 = Read-Host "Número 1"
+[double]$num2 = Read-Host "Número 2"
+
+$saida = switch ($operacao.Trim().ToLower()) {
+    "soma"       { $num1 + $num2 }
+    "subtrai"    { $num1 - $num2 }
+    "multiplica" { $num1 * $num2 }
+    "divide"     { 
+        if ($num2 -eq 0) { "Erro: Divisão por zero" } 
+        else { $num1 / $num2 }
+    }
+    default      { "Operação inválida" }
+}
+
+Write-Output $saida
